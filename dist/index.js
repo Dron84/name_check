bc93c49baefe3316e386801a3e784a069ce02019
dron84 <dron84@gmail.com>
2023-03-01 16:12:12 +0300

    update

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.nameCheck = void 0;
 const getName_1 = require("./getName");
 const getNameCombination_1 = require("./getNameCombination");
-const nameCheck = ({ player, squadList, playerNameField, playerShirtnumberField, squadNameField, squadShirtnumberField, squadListFieldReturn }) => {
+const nameCheck = ({ player, squadList, playerNameField, playerShirtnumberField, squadNameField, squadShirtnumberField, squadListFieldReturn, }) => {
     if (squadList) {
-        let price = "0";
+        let price = '0';
         //@ts-ignore
         const playerName = (0, getName_1.getName)(player[playerNameField]);
-        squadList.forEach(item => {
+        squadList.forEach((item) => {
             //@ts-ignore
             const itemName = (0, getName_1.getName)(item[squadNameField]);
             if (playerName === itemName) {
@@ -16,10 +16,10 @@ const nameCheck = ({ player, squadList, playerNameField, playerShirtnumberField,
                 price = item[squadListFieldReturn];
             }
         });
-        if (price == "0") {
+        if (price == '0') {
             //@ts-ignore
             const playerName = (0, getNameCombination_1.getNameCombination)(player[playerNameField]);
-            squadList.forEach(item => {
+            squadList.forEach((item) => {
                 //@ts-ignore
                 const itemName = (0, getNameCombination_1.getNameCombination)(item[squadNameField]);
                 if (playerName[0] === itemName[0] && playerName[1] === itemName[1]) {
@@ -30,25 +30,31 @@ const nameCheck = ({ player, squadList, playerNameField, playerShirtnumberField,
                     //@ts-ignore
                     price = item[squadListFieldReturn];
                 }
-                if (playerName[0].slice(0, 1) === itemName[0].slice(0, 1) && playerName[1] === itemName[1]) {
+                if (playerName[0].slice(0, 1) === itemName[0].slice(0, 1) &&
+                    playerName[1] === itemName[1]) {
                     //@ts-ignore
                     price = item[squadListFieldReturn];
                 }
-                if (playerName[1].slice(0, 1) === itemName[0].slice(0, 1) && playerName[0] === itemName[1]) {
-                    //@ts-ignore
-                    price = item[squadListFieldReturn];
-                }
-            });
-        }
-        if (price == "0") {
-            squadList.forEach(item => {
-                //@ts-ignore
-                if (parseInt(player[playerShirtnumberField]) == parseInt(item[squadShirtnumberField])) {
+                if (playerName[1].slice(0, 1) === itemName[0].slice(0, 1) &&
+                    playerName[0] === itemName[1]) {
                     //@ts-ignore
                     price = item[squadListFieldReturn];
                 }
             });
         }
+        // if (price == '0') {
+        //   squadList.forEach((item) => {
+        //     if (
+        //       //@ts-ignore
+        //       parseInt(player[playerShirtnumberField]) ==
+        //       //@ts-ignore
+        //       parseInt(item[squadShirtnumberField])
+        //     ) {
+        //       //@ts-ignore
+        //       price = item[squadListFieldReturn]
+        //     }
+        //   })
+        // }
         return price;
     }
 };
